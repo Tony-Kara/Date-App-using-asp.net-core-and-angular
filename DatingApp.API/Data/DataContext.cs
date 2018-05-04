e387b26019c33a3dda24becd528bf3cef400786b
Eniola Anthony <eniolaanthony@gmail.com>
2018-05-04 20:46:21 +0100

    Initial Commit

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,15 @@
+using DatingApp.API.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
---
+++ DatingApp.API/Model/Value.cs
@@ -0,0 +1,8 @@
+namespace DatingApp.API.Model
+{
+    public class Value
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }   
+    }
+}
